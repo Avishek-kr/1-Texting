9ff1bb63c8543bee56c5e99004123feb43dda2e8
Abhishek Kumar <ak251997@gmail.com>
2021-08-28 22:21:23 +0530

    enable dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import AboutUS from './components/AboutUS';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar title="Textutils" />
         <div className="container my-3">
       <TextForm heading="Enter the text to analyse" />
+      <AboutUS />
         </div>
     </>
   );
