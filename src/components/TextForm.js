fbebf5dc0ca9192162e50ac9fd992cfd9570c258
Abhishek Kumar <ak251997@gmail.com>
2021-08-28 20:57:54 +0530

    words and character

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,7 +15,8 @@ function TextForm(props) {
         setText(e.target.value)
     }
     return (
-        <div>
+        <>
+        <div className="container">
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
@@ -23,6 +24,12 @@ function TextForm(props) {
             <button onClick={()=>UpperCase()} className="btn btn-primary">Convert to Uppercase</button>
             <button onClick={()=>LowerCase()} className="btn btn-primary mx-3">Convert to Lowercase</button>
         </div>
+        <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+
+        </div>
+        </>
     )
 }
 
